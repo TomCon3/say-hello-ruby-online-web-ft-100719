e9a77d61b84340178b6478636d9581198433cda4
Tommy Connor <tconnor98@aol.com>
2019-10-17 19:41:21 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,13 +1,12 @@
 # Build your say_hello method here
-def say_hello(name)
+def say_hello 
+  puts "Hello Ruby Programmer!"
+end
+
+say_hellodef say_hello(name)
   puts "Hello, #{name}!"
 end
 
 say_hello("Gabriela")
 
 
-def say_hello 
-  puts "Hello Ruby Programmer!"
-end
-
-say_hello
