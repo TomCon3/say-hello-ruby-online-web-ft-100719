af444ecd0c08acd298185c0949633f5da51f48c5
Tommy Connor <tconnor98@aol.com>
2019-10-17 19:42:00 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,13 +1,13 @@
-def say_hello 
+def say_hello(name)
+  puts "Hello, #{name}!"
+end
+
+say_hello("Gabriela")def say_hello 
   puts "Hello Ruby Programmer!"
 end
 
 say_hello
 
-def say_hello(name)
-  puts "Hello, #{name}!"
-end
 
-say_hello("Gabriela")
 
 
